d157075913e52844d19dd0343b4c9f8baa9e0479
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-12-26 18:15:09 -0600

    Updated map

--- js/index.js
+++ js/index.js
@@ -1,4 +1,72 @@
-function showData(state) {
+/* function showData(state) {
   alert("Data for " + state);
   // You can replace the alert with your logic to display data
 }
+*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get all circle elements
+  var circles = document.querySelectorAll("circle");
+
+  // Add event listeners to each circle
+  circles.forEach(function (circle) {
+    circle.addEventListener("mouseover", function () {
+      showData(circle.getAttribute("data-state"), circle);
+    });
+
+    circle.addEventListener("click", function () {
+      showData(circle.getAttribute("data-state"), circle);
+    });
+  });
+
+  function showData(state, circle) {
+    // Get the specific data for the state (replace this with your actual data logic)
+    var dataForState = getDataForState(state);
+
+    console.log(state);
+    console.log("Data for state:", dataForState);
+
+    // Create a message element
+    var message = document.createElement("div");
+    message.className = "message";
+    message.textContent = "Data for " + state + ": " + dataForState;
+
+    console.log("Message element:", message);
+
+    // Position the message element above the circle
+    var circleRect = circle.getBoundingClientRect();
+    message.style.top = circleRect.top - 30 + "px"; // Adjust the position as needed
+    message.style.left = circleRect.left + "px";
+
+    console.log("Top of circle:", circleRect.top);
+    console.log("Left of circle:", circleRect.left);
+    console.log("Calculated top for message:", circleRect.top - 30 + "px");
+    console.log("Calculated left for message:", circleRect.left + "px");
+
+    // Append the message element to the body
+    document.body.appendChild(message);
+
+    // Remove the message element after a delay (e.g., 2 seconds)
+    setTimeout(function () {
+      document.body.removeChild(message);
+    }, 7000);
+  }
+
+  // Replace this function with your actual data retrieval logic
+  function getDataForState(state) {
+    console.log("Getting data for state:", state);
+    // Example: Replace this with your actual data retrieval logic
+    var data = {
+      CDMX: "Population: 5 million, Capital: Chihuahua City",
+      Guadalajara: "Population: 8 million, Capital: Guadalajara",
+      Monterrey: "Population: 5 million, Capital: Chihuahua City",
+      Puebla: "Population: 8 million, Capital: Guadalajara",
+      Cancun: "Population: 5 million, Capital: Chihuahua City",
+      Villahermosa: "Population: 8 million, Capital: Guadalajara",
+      Queretaro: "Population: 5 million, Capital: Chihuahua City",
+      Veracruz: "Population: 8 million, Capital: Guadalajara",
+    };
+
+    return data[state] || "No data available";
+  }
+});
