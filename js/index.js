69ac21eb4d9ad9d02aa9cb81b81145a09066cfbe
Mariana Téllez <tellezresendizmariana@gmail.com>
2023-12-28 11:23:55 -0600

    Updated click functionality

--- js/index.js
+++ js/index.js
@@ -1,6 +1,162 @@
 $(document).ready(function () {
   $(".hide").hide();
 
+  // Keep track of the visibility state for each marker
+  var markerStates = {
+    cdmx: false,
+    guadalajara: false,
+    monterrey: false,
+    puebla: false,
+    cancun: false,
+    villahermosa: false,
+    queretaro: false,
+    veracruz: false,
+  };
+
+  function showText(element) {
+    $(".hide").hide();
+    $(element).fadeIn(250);
+  }
+
+  function hideText() {
+    $(".hide").hide();
+  }
+
+  function toggleText(marker, textClass) {
+    if (markerStates[marker]) {
+      hideText();
+      markerStates[marker] = false;
+    } else {
+      showText(textClass);
+      markerStates[marker] = true;
+    }
+  }
+
+  $(".marker-cdmx")
+    .click(function () {
+      toggleText("cdmx", ".cdmx-text");
+    })
+    .hover(
+      function () {
+        showText(".cdmx-text");
+      },
+      function () {
+        if (!markerStates.cdmx) {
+          hideText();
+        }
+      }
+    );
+
+  $(".marker-guadalajara")
+    .click(function () {
+      toggleText("guadalajara", ".guadalajara-text");
+    })
+    .hover(
+      function () {
+        showText(".guadalajara-text");
+      },
+      function () {
+        if (!markerStates.guadalajara) {
+          hideText();
+        }
+      }
+    );
+
+  $(".marker-monterrey")
+    .click(function () {
+      toggleText("monterrey", ".monterrey-text");
+    })
+    .hover(
+      function () {
+        showText(".monterrey-text");
+      },
+      function () {
+        if (!markerStates.monterrey) {
+          hideText();
+        }
+      }
+    );
+
+  $(".marker-puebla")
+    .click(function () {
+      toggleText("puebla", ".puebla-text");
+    })
+    .hover(
+      function () {
+        showText(".puebla-text");
+      },
+      function () {
+        if (!markerStates.puebla) {
+          hideText();
+        }
+      }
+    );
+
+  $(".marker-cancun")
+    .click(function () {
+      toggleText("cancun", ".cancun-text");
+    })
+    .hover(
+      function () {
+        showText(".cancun-text");
+      },
+      function () {
+        if (!markerStates.cancun) {
+          hideText();
+        }
+      }
+    );
+
+  $(".marker-villahermosa")
+    .click(function () {
+      toggleText("villahermosa", ".villahermosa-text");
+    })
+    .hover(
+      function () {
+        showText(".villahermosa-text");
+      },
+      function () {
+        if (!markerStates.villahermosa) {
+          hideText();
+        }
+      }
+    );
+
+  $(".marker-queretaro")
+    .click(function () {
+      toggleText("queretaro", ".queretaro-text");
+    })
+    .hover(
+      function () {
+        showText(".queretaro-text");
+      },
+      function () {
+        if (!markerStates.queretaro) {
+          hideText();
+        }
+      }
+    );
+
+  $(".marker-veracruz")
+    .click(function () {
+      toggleText("veracruz", ".veracruz-text");
+    })
+    .hover(
+      function () {
+        showText(".veracruz-text");
+      },
+      function () {
+        if (!markerStates.veracruz) {
+          hideText();
+        }
+      }
+    );
+});
+
+/*
+$(document).ready(function () {
+  $(".hide").hide();
+
   function showText(element) {
     $(".hide").hide();
     $(element).fadeIn(250);
@@ -114,3 +270,5 @@ $(document).ready(function () {
       }
     );
 });
+
+*/
